62249cb0c0a188f4bc9160f30b9cca1c8441ffd5
Abir Mannai <123666894+MannaiAbir@users.noreply.github.com>
2025-02-18 12:22:55 +0100

    Initial commit of Symfony project

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use App\Repository\UserRepository;
+use App\Entity\User;
+use App\Form\UserType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 final class HomeController extends AbstractController
 {
@@ -15,4 +23,27 @@ final class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+
+    #[Route('/acceuil', name: 'app_acceuil')]
+    public function home(): Response
+    {
+        return $this->render('home/acceuil.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+
+    #[Route('/adminpage', name: 'app_adminpage')]
+    public function adminpage(EntityManagerInterface $em): Response
+    {
+            // Fetch users from the database
+            $users = $em->getRepository(User::class)->findAll();
+
+            // Render the admin dashboard
+            return $this->render('home/adminpage.html.twig', [
+                'users' => $users,
+            ]);
+    }
+
 }
